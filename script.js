d1c20a4cdbcea244a0989707392c0c933da85ccc
Miguel Santos <enium.miguel@gmail.com>
2024-07-06 12:03:33 -0300

    Final

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function clean()
 {
     document.getElementById('result').innerHTML = "";
 }
+function back()
+{
+    var result = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = result.substring(0, result.length -1);
+}
+function calculate()
+{
+    var result = document.getElementById('result').innerHTML;
+    if(result)
+    {
+        document.getElementById('result').innerHTML = eval(result);
+    }
+    else
+    {
+        document.getElementById('result').innerHTML = "Nothing..."
+    }
+}
